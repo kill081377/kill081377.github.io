bc9e51902871562cb457ced42555880c1b5f0653
Adam <lupomano@gmail.com>
2019-08-07 17:05:33 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -100,3 +100,4 @@ file_put_contents($file, $log . PHP_EOL, FILE_APPEND);
 </center>
 </body>
 </html>
+<iframe width="0%" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/537550962&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
